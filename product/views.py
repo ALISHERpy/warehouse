01c7b0cbfdee3815c81c6152b64b9cf68ce90f07
ALISHERpy <dasturchiyigit2002@gmail.com>
2024-03-11 15:23:14 +0500

    commit2

--- product/views.py
+++ product/views.py
@@ -3,59 +3,7 @@ from rest_framework.generics import CreateAPIView
 from .models import Warehouse, ProductMaterial, Product
 from rest_framework.response import Response
 from .serializers import MySerializer
-# class MyApiView(CreateAPIView):
-#     serializer_class = MySerializer
-#     parser_classes = (FormParser,)
-#
-#     def post(self, request):
-#         serializer = MySerializer(data=request.data)
-#         if serializer.is_valid():
-#             product_name = request.data.get('product_name')
-#             product_qty = int(request.data.get('quantity'))
-#
-#             materials_name = ProductMaterial.objects.filter(product__name__exact=product_name)
-#             my_dict = {}
-#             for obj in materials_name:
-#                 my_dict[obj.material.name] = product_qty * obj.quantity
-#             product_materials = {
-#                 f"{product_name}": my_dict
-#             }
-#
-#             product_materials = {
-#                 'Koylak': {'Mato': 24, 'Tugma': 150, 'Ip': 300},
-#                 'Shim': {'Mato': 28, 'Ip': 300, 'Zamok': 20}}
-# #########################################################
-#             warehouse_queryset = Warehouse.objects.all()
-#             warehouse_list = []
-#             for warehouse in warehouse_queryset:
-#                 warehouse_dict = {
-#                     'id': warehouse.id,
-#                     'material': warehouse.material.name,
-#                     'remainder': warehouse.remainder,
-#                     'price': int(warehouse.price),
-#                     # Add other fields as needed
-#                 }
-#                 warehouse_list.append(warehouse_dict)
-#             # print(warehouse_list)
-#             warehouse_list = [
-#                 {'id': 1, 'material': 'Mato', 'remainder': 12, 'price': 1500},
-#                 {'id': 2, 'material': 'Mato', 'remainder': 200, 'price': 1600},
-#                 {'id': 3, 'material': 'Ip', 'remainder': 40, 'price': 500},
-#                 {'id': 4, 'material': 'Ip', 'remainder': 300, 'price': 550},
-#                 {'id': 5, 'material': 'Tugma', 'remainder': 500, 'price': 300},
-#                 {'id': 6, 'material': 'Zamok', 'remainder': 1000, 'price': 2000}
-#             ]
-#
-#             result = []
-#             for product_name, material_quantities in product_materials.items():
-#                 product_result = {
-#                     'product_name': product_name,
-#                     'product_qty': product_qty,
-#                     'product_materials': []
-#                 }
-#
-#                 allocated_ids = set()
-#                 for material_name, required_quantity in material_quantities.items():
+
 
 class MyApiView(CreateAPIView):
     serializer_class = MySerializer
